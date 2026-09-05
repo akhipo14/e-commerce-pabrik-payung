cead76bc6adc5bd4ad4e3d864059ca4343348829
ajahaikal341 <raflihaikal14122000@gmail.com>
2026-09-05 18:44:39 +0700

    update

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ lihat();  //menjalankan function lihat
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css">
+  <meta name="google-site-verification" content="eYiYHyGUVVcmYdnR_qp9NsUsJG_zInnV58YnzntTX9c" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
   <script src="https://unpkg.com/scrollreveal"></script>
