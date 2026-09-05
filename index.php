4dc0ce6c868f5aaa65fefa98caaa9d5378e1b6f2
ajahaikal341 <raflihaikal14122000@gmail.com>
2026-09-05 18:38:48 +0700

    update

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ lihat();  //menjalankan function lihat
     </div>
     <!-- tombol-whatsapp -->
     <div class="tombol-whatsapp" style="z-index:2;">
-      <a href="https://wa.me/6281214691427?text=Halo,%20saya%20mau%20tanya-tanya%20tentang%20produk%20payung%20anda.><i class=" fa-brands fa-whatsapp"></i></a>
+      <a href="https://wa.me/6281214691427?text=Halo,%20saya%20mau%20tanya-tanya%20tentang%20produk%20payung%20anda."><i class=" fa-brands fa-whatsapp"></i></a>
     </div>
     <!-- end-tombol-whatsapp -->
   </div>
